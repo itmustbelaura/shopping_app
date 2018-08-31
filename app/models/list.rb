2595b9a5e6b3fbb7f485c80de5ceffffaf0996a9
Laura Alvarez <itmustbelaura@gmail.com>
2018-08-30 19:02:48 -0600

    added more items

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,4 @@
 class List < ApplicationRecord
     belongs_to :store
-    has_many :items
+    has_many :items, dependent: :destroy
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -5,7 +5,7 @@ class Store < ApplicationRecord
     validates :phone
     validates :email
     #associations
-    has_many :list
+    has_many :list, dependent: :destroy
     #callbacks
     after_validation :save
     #Instance / Class methods
